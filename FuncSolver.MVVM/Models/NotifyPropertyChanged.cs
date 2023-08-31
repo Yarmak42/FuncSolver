ae2929c7323089741eae165c82ec73e1f98cf1af
Ярослав Максимов <yarmak200105@gmail.com>
2023-08-31 15:40:49 +0700

    fix(FuncSolver.MVVM.Models): ограничен ввод коэффициентов только цифрами; при изменении Х и Y значение функции F меняется сразу.

---
+++ FuncSolver.MVVM/Models/NotifyPropertyChanged.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FuncSolver.MVVM
+{
+    /// <summary>
+    /// Реализация интерфейса INotifyPropertyChanged. Извещает систему
+    /// об изменении свойства.
+    /// </summary>
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
